c051c026f2e7b8fe5ffc1f9fa041e37ea09deb22
zhangpengju <13290505500@163.com>
2019-04-04 15:52:57 +0800

    提交library

---
+++ net_service_library/src/main/java/com/zpj/sdk/net_service_library/TestLibrary.kt
@@ -0,0 +1,12 @@
+package com.zpj.sdk.net_service_library
+
+/**
+ * Created by zhangpengju on 2019/4/4.
+ */
+object  TestLibrary {
+
+    fun  getString(): String{
+        return "zpj_net_service"
+    }
+
+}
